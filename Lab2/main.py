459eaf1f582cdfd2fe4e831a6f4bf57838038b8e
Alex <olexandrpasalsky@gmail.com>
2023-02-27 23:21:16 +0200

    Task2

--- Lab2/main.py
+++ Lab2/main.py
@@ -40,6 +40,26 @@ def found_smile_eye_faces(path_image, scaling_factor):
     cv2.imshow("Image", frame)
     cv2.waitKey(0)
 
+def found_faces_video(path_video):
+    cv2.startWindowThread()
+
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    cap = cv2.VideoCapture(path_video)
+    while True:
+        ret, frame = cap.read()
+        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
+
+        cv2.imshow("Video", frame)
+        if cv2.waitKey(1) & 0XFF==ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 if __name__ == '__main__':
-    found_smile_eye_faces("1.jfif", 3)
-    found_smile_eye_faces("2.jfif", 4)
+    found_smile_eye_faces("media\\1.jfif", 3)
+    found_smile_eye_faces("media\\2.jfif", 4)
+
+    found_faces_video("media\\video.mp4")
