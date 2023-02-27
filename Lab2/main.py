5018cbf698bf7f3b5e4055a48fe16d3ae06033f0
Alex <olexandrpasalsky@gmail.com>
2023-02-28 00:10:42 +0200

    Task3

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy
 
 def found_faces(path_image, scaling_factor):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
@@ -58,8 +59,38 @@ def found_faces_video(path_video):
     cap.release()
     cv2.destroyAllWindows()
 
+def found_people(path_video):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    cv2.startWindowThread()
+    cap = cv2.VideoCapture(path_video)
+    while True:
+        ret, frame = cap.read()
+
+        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
+
+        boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
+        boxes = numpy.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+        for (xa, ya, xb, yb) in boxes:
+            cv2.rectangle(frame, (xa, ya), (xb, yb), (0, 255, 255), 1)
+
+        cv2.imshow("Video", frame)
+        if cv2.waitKey(1) & 0XFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
     found_smile_eye_faces("media\\1.jfif", 3)
     found_smile_eye_faces("media\\2.jfif", 4)
 
     found_faces_video("media\\video.mp4")
+
+    found_people("media\\video2.mp4")
