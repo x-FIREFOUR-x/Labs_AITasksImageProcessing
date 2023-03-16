811c1902d792c15fc90e1de8cebc89c15ec2155e
Alex <olexandrpasalsky@gmail.com>
2023-03-16 20:40:39 +0200

    finish

--- Lab4/face_recognition_images.py
+++ Lab4/face_recognition_images.py
@@ -3,31 +3,45 @@ import cv2
 
 from utils import face_encodings, nb_of_matches, face_rects
 
+
 with open("encodings.pickle", "rb") as f:
     name_encodings_dict = pickle.load(f)
 
-image = cv2.imread("data/examples/2.jpg")
 
-encodings = face_encodings(image)
-names = []
+def recognition_image(path):
+    image = cv2.imread(path)
 
-for encoding in encodings:
-    counts = {}
-    for (name, known_encodings) in name_encodings_dict.items():
-        counts[name] = nb_of_matches(known_encodings, encoding)
+    encodings = face_encodings(image)
+    names = []
 
-    if all(count == 0 for count in counts.values()):
-        name = "Unknown"
-    else:
-        name = max(counts, key=counts.get)
+    for encoding in encodings:
+        counts = {}
+        for (name, known_encodings) in name_encodings_dict.items():
+            counts[name] = nb_of_matches(known_encodings, encoding)
 
-    names.append(name)
+        if all(count == 0 for count in counts.values()):
+            name = "Unknown"
+        else:
+            name = max(counts, key=counts.get)
 
-for rect, name in zip(face_rects(image), names):
-    x1, y1, x2, y2 = rect.left(), rect.top(), rect.right(), rect.bottom()
-    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    cv2.putText(image, name, (x1, y1 - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+        names.append(name)
 
-    cv2.imshow("image", image)
-    cv2.waitKey(0)
+    for rect, name in zip(face_rects(image), names):
+        x1, y1, x2, y2 = rect.left(), rect.top(), rect.right(), rect.bottom()
+
+        if name == "Unknown":
+            color = (0, 0, 255)
+        else:
+            color = (0, 255, 0)
+
+        cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        cv2.putText(image, name, (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
+
+        cv2.imshow("image", image)
+        cv2.waitKey(0)
+
+
+recognition_image("data/examples/1.jpg")
+recognition_image("data/examples/3.jpg")
+recognition_image("data/examples/4.jpeg")
