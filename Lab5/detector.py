efbaa482cbbf4aaa1ff12c07f43164afb3a28148
Alex <olexandrpasalsky@gmail.com>
2023-03-18 18:41:37 +0200

    detect object in image and video

--- Lab5/detector.py
+++ Lab5/detector.py
@@ -1,17 +1,16 @@
 from imageai.Detection import ObjectDetection
 
-
 model_path = "models/yolo-tiny.h5"
-input_path = "data/input/test_im.jpg"
-output_path = "data/output/test_im.jpg"
-
 detector = ObjectDetection()
 detector.setModelTypeAsTinyYOLOv3()
 
 detector.setModelPath(model_path)
 detector.loadModel()
-detection = detector.detectObjectsFromImage(
-    input_image=input_path, output_image_path=output_path)
 
-for eachItem in detection:
-    print(f"{eachItem['name']}:{eachItem['percentage_probability']}")
+def detect_objects(input_path, output_path, print_object_flg=False):
+    detection = detector.detectObjectsFromImage(
+        input_image=input_path, output_image_path=output_path)
+
+    if print_object_flg:
+        for eachItem in detection:
+            print(f"{eachItem['name']}:{eachItem['percentage_probability']}")
--- Lab5/main.py
+++ Lab5/main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import cv2
+import imutils
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from detector import detect_objects
 
 
-# Press the green button in the gutter to run the script.
+def detect_in_image(input_path, output_path, scale = 1):
+    detect_objects(input_path, output_path)
+
+    img = cv2.imread(output_path)
+    h, w = img.shape[0:2]
+    img = imutils.resize(img, height=h * scale)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+
+
+def detect_in_video(input_path):
+    video_capture = cv2.VideoCapture(input_path)
+    input_buf = "data/buffer/buf.jpg"
+    output_buf = "data/buffer/buf.jpg"
+
+    while video_capture.isOpened():
+        ret, frame = video_capture.read()
+        cv2.imwrite(input_buf, frame)
+        if ret:
+            detect_objects(input_buf, output_buf)
+            frame = cv2.imread(output_buf)
+            cv2.imshow("video", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #detect_in_image("data/input/test_im.jpg", "data/output/test_im.jpg")
+    detect_in_image("data/input/fruit2.jpeg", "data/output/fruit2.jpg", 2)
+    detect_in_video("data/input/video.mp4")
