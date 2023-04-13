3ae545e73c0b8dc54fac528e3db905e3e93e757e
Alex <olexandrpasalsky@gmail.com>
2023-04-13 23:10:40 +0300

    model

--- Lab6/main.py
+++ Lab6/main.py
@@ -53,3 +53,35 @@ if __name__ == '__main__':
                       .map(process_images)
                       .shuffle(buffer_size=train_ds_size)
                       .batch(batch_size=32, drop_remainder=True))
+
+    model = keras.models.Sequential([
+        keras.layers.Conv2D(filters=96, kernel_size=(11, 11),
+                            strides=(4, 4), activation='relu',
+                            input_shape=(227, 227, 3)),
+        keras.layers.BatchNormalization(),
+        keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+        keras.layers.Conv2D(filters=256, kernel_size=(5, 5),
+                            strides=(1, 1), activation='relu',
+                            padding="same"),
+        keras.layers.BatchNormalization(),
+        keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+        keras.layers.Conv2D(filters=384, kernel_size=(3, 3),
+                            strides=(1, 1), activation='relu',
+                            padding="same"),
+        keras.layers.BatchNormalization(),
+        keras.layers.Conv2D(filters=384, kernel_size=(3, 3),
+                            strides=(1, 1), activation='relu',
+                            padding="same"),
+        keras.layers.BatchNormalization(),
+        keras.layers.Conv2D(filters=256, kernel_size=(3, 3),
+                            strides=(1, 1), activation='relu',
+                            padding="same"),
+        keras.layers.BatchNormalization(),
+        keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+        keras.layers.Flatten(),
+        keras.layers.Dense(4096, activation='relu'),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(4096, activation='relu'),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(10, activation='softmax')
+    ])
